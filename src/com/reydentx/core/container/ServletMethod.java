3f21af837ea10e360e4b2ca0c19055dbb47d2287
tienducitt <tienducitt@gmail.com>
2017-02-23 10:07:43 +0700

    add MysqlClient and example

--- src/com/reydentx/core/container/ServletMethod.java
+++ src/com/reydentx/core/container/ServletMethod.java
@@ -44,7 +44,7 @@ public class ServletMethod {
         }
 
         public void buildFullPath() {
-                String fp = RWebServer.getINSTANCE().getContextPath() + "/" + servletObject.getServletPath() + "/" + url;
+                String fp = RWebServer.getINSTANCE().getContextPath() + "/" + servletObject.getServletPath() + url;
                 fp = RUtil.replaceAllDupSlash(fp);
                 this.fullPath = fp;
         }
